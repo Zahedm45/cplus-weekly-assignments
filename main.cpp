88607d613f2f0a27a4bcbe02b879c9a782e6796c
Zahed <zahedm45@gmail.com>
2022-09-26 23:20:33 +0200

    Refactored some of the types.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <vector>
 #include "week4.cpp"
+#include "week1.cpp"
+
 
 using namespace std;
 
 int main() {
 
-
+    week4();
     return 0;
 }
 
--- week4.cpp
+++ week4.cpp
@@ -1,12 +1,18 @@
 //
 // Created by MD. Zahed on 26/09/2022.
 //
+using namespace std;
 
-typedef enum { wood, stone } material;
-struct tile {
+typedef enum {
+    WOOD,
+    STONE
+
+} Material;
+
+struct Tile {
     int x, y;
     bool isWall;
-    material type;
+    Material type;
 };
 
 
@@ -14,18 +20,22 @@ struct tile {
 #define NCOLS 16
 
 
-int main2() {
-    tile playground[NROWS][NCOLS];
+int week4() {
+
+
+    Tile playground[NROWS][NCOLS];
     for (int i = 0; i < NROWS; i++) {
         for (int j = 0; j < NCOLS; j++) {
             playground[i][j].x = j;
             playground[i][j].y = i;
             playground[i][j].isWall = (j==0 || i==(NROWS-1) || (i==0 && j!=3) || j==(NCOLS-1));
             if (playground[i][j].isWall) {
-                playground[i][j].type = stone;
+                playground[i][j].type = STONE;
             } else {
-                playground[i][j].type = wood;
+                playground[i][j].type = WOOD;
             }
         }
     }
+
+    return 0;
 }
