7b72f1e25fe43cb539cfd869ea814fbdd3814829
Zahed <zahedm45@gmail.com>
2022-09-27 11:10:44 +0200

    First part of the assignment is now done and passed it on Codejudge.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <vector>
 #include "week4.cpp"
-#include "week1.cpp"
+
 
 
 using namespace std;
+/*
 
 int main() {
 
     week4();
     return 0;
 }
+*/
 
--- week4.cpp
+++ week4.cpp
@@ -1,6 +1,10 @@
 //
 // Created by MD. Zahed on 26/09/2022.
 //
+#include <iostream>
+#include <vector>
+
+
 using namespace std;
 
 typedef enum {
@@ -28,7 +32,7 @@ typedef struct Player{
 
 
 void display(Tile pTile[NROWS][NCOLS], Player player);
-
+bool is_move_possible(Tile pTile[NROWS][NCOLS], Player player);
 
 int week4() {
 
@@ -39,6 +43,7 @@ int week4() {
             playground[i][j].x = j;
             playground[i][j].y = i;
             playground[i][j].isWall = (j==0 || i==(NROWS-1) || (i==0 && j!=3) || j==(NCOLS-1));
+
             if (playground[i][j].isWall) {
                 playground[i][j].type = STONE;
             } else {
@@ -49,12 +54,87 @@ int week4() {
 
 
     Player player = {5, 5};
-
     display(playground, player);
 
 
+    while (true) {
+        char user_input;
+        cin >> user_input;
+        Player new_player;
+
+
+        switch (user_input) {
+            case 'q':
+                exit(0);
+
+            case 'l':
+
+                new_player = {player.x - 1, player.y};
+                if (is_move_possible(playground, new_player)) {
+                    player = new_player;
+                }
+                break;
+
+            case 'r':
+                new_player = {player.x + 1, player.y};
+                if (is_move_possible(playground, new_player)) {
+                    player = new_player;
+                }
+                break;
+
+            case 'u':
+
+                new_player = {player.x, player.y - 1};
+                if (is_move_possible(playground, new_player)) {
+                    player = new_player;
+                }
+                break;
+
+            case 'd':
+                new_player = {player.x, player.y + 1};
+                if (is_move_possible(playground, new_player)) {
+                    player = new_player;
+                }
+                break;
+            default:
+                break;
+        }
+
+
+        display(playground, player);
+    }
+
 
     return 0;
+}
+
+/*int main() {
+    week4();
+    return 0;
+}*/
+
+
+
+
+bool is_move_possible(Tile pTile[NROWS][NCOLS], Player player) {
+
+    int px = player.x;
+    int py = player.y;
+
+
+    if (px < 0 || py < 0 || px > NCOLS-1 || py > NROWS-1 ) {
+        return false;
+
+    }
+
+    if (!pTile[py][px].isWall) {
+        return true;
+    }
+
+
+
+    return false;
+
 }
 
 void display(Tile pTile[NROWS][NCOLS], Player player) {
