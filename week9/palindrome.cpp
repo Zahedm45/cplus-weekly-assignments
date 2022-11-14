023d6cf56b881f62d14f084261274ad54e8e9f4f
Zahed <zahedm45@gmail.com>
2022-11-14 19:18:11 +0100

    Palindrome done!

--- week9/palindrome.cpp
+++ week9/palindrome.cpp
@@ -1,41 +1,59 @@
 //
 // Created by MD. Zahed on 14/11/2022.
 //
+
+
+//
+// Created by MD. Zahed on 14/11/2022.
+//
 #include <string>
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-int main_reverse() {
+int main() {
     string str;
     getline(cin, str);
 
 
     size_t pos = 0;
     string token;
-    vector<string> vectorStr;
+    vector<int> vectorStr;
     string delimiter = " ";
 
     while ((pos = str.find(delimiter)) != string::npos) {     // string::npos = -1
         token = str.substr(0, pos);
-        vectorStr.push_back(token);
+        vectorStr.push_back(stoi(token));
         str.erase(0, pos + delimiter.length());
     }
 
     token = str.substr(0, pos);     // The last element
-    vectorStr.push_back(token);
+    vectorStr.push_back(stoi(token));
 
 
 
 
     int i = vectorStr.size()-1;
+    int k = 0;
 
     while (i >= 0) {
 
-        cout << vectorStr[i] << " ";
+        int rto = vectorStr[i];
+        int lto = vectorStr[k];
+
+
+        if (rto != lto) {
+            cout << "no";
+            return 0;
+        }
 
         i--;
+        k++;
     }
+
+
+
+    cout << "yes";
     return 0;
 }
