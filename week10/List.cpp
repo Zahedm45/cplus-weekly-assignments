3207a4241d76aa2f2f2495ff7c86275537bad256
Zahed <zahedm45@gmail.com>
2022-11-21 23:40:56 +0100

    Week 10 done

--- week10/List.cpp
+++ week10/List.cpp
@@ -2,6 +2,7 @@
 // Created by MD. Zahed on 21/11/2022.
 //
 
+#include <vector>
 #include "List.h"
 
 
@@ -11,6 +12,13 @@ List::List() {
 
 List::~List() {
 
+    Node *temp;
+    while (first != NULL){
+
+        temp = first;
+        first = first->next;
+        delete temp;
+    }
 }
 
 void List::insert(int n) {
@@ -49,21 +57,49 @@ void List::print() {
 
 void List::reverse() {
 
+    vector<int> tempArr;
+
+    if (first == NULL) {
+        return;
+    }
+
+    Node *temp = first;
+    while (temp != NULL) {
+        tempArr.push_back(temp->val);
+        temp = temp->next;
+    }
+
+    temp = first;
+    int n = (int) tempArr.size();
+
+    for (int i = (n - 1); i >= 0; --i) {
+
+        if (temp == NULL) {
+            break;
+        }
+
+        temp->val = tempArr[i];
+        temp = temp->next;
+
+    }
+
+
 }
 
 
+/*
 int main() {
 
     List list;
 
     list.insert(1);
-    list.insert(1);
-    list.insert(1);
-    list.insert(1);
-
+    list.insert(2);
+    list.insert(3);
+    list.insert(4);
+    list.print();
+    list.reverse();
     list.print();
 
 
 
-
-}
+}*/
