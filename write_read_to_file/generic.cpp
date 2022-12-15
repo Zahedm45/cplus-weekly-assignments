bb74c5fad1b0267e45fd0ae15e5546f8225e2af3
Zahed <zahedm45@gmail.com>
2022-12-15 22:01:13 +0100

    Generic type

---
+++ write_read_to_file/generic.cpp
@@ -0,0 +1,20 @@
+//
+// Created by MD. Zahed on 15/12/2022.
+//
+using namespace std;
+#include <iostream>
+
+
+template <class T>
+T find_max(T a, T b) {
+    return (a > b)? a:b;
+}
+
+
+int main() {
+
+    double val = find_max(10.3, 11.4);
+    cout << val << endl;
+
+    return 0;
+}
