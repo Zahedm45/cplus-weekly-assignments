c5accc933de0359f17fa4ea95a0a64a5c482bb27
Zahed <zahedm45@gmail.com>
2022-12-18 10:14:17 +0100

    Went back to the normal file structure

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include "week4/part2_4.cpp"
+#include <fstream>
+using namespace std;
+
+
+
+
+
+
+int main() {
+    cout << "helloddd" << endl;
+    return 0;
+}
+
